ebe77a2e2bf77b961bdf14c22033f7d22f904c3e
Carlos Lucero <nandolucero@icloud.com>
2023-01-13 22:46:22 -0500

    v3

--- grafico_3.js
+++ grafico_3.js
@@ -1,7 +1,7 @@
 d3.json(
     "https://raw.githubusercontent.com/OCNVN/test/main/data.json"
   ).then(function (datos) {
-    var data = datos;
+    var data = datos.sort((a, b) => a.nota - b.nota);
     // var data = data.sort((a, b) => a.nota - b.nota);
     var color = d3.scaleOrdinal().range(d3.schemeSet3);
     var width = 600;
@@ -24,6 +24,16 @@ d3.json(
     .domain([0, d3.max(data, (d) => d.ranking)]) // Ranking
     .range([height, 0]);
 
+    // Escalador para color
+    var colorScale = d3.scaleLinear()
+    .domain([0, d3.max(data, (d) => d.ranking)])
+    .range([0, 255]);
+
+    // Escalador para radio
+    var radioScale = d3.scaleLinear()
+    .domain([0, d3.max(data, (d) => d.ranking)])
+    .range([3, 15]);
+
     // Crear eje X
     svg.append("g")
     .attr("class", "x axis")
@@ -41,96 +51,40 @@ d3.json(
     .y(function(d) { return yScale(d.ranking); }) // Valores Y
     .curve(d3.curveMonotoneX) // Aplicar suavizador
 
-    // Dibujar linea
-    svg.append("path")
-    .datum(data) // Enlaza datos con la linea
-    .attr("class", "line-salida") // Estilos
-    .attr("d", lineSalida); // Llamar al generador de lineas
+    // // Dibujar linea
+    // svg.append("path")
+    // .datum(data) // Enlaza datos con la linea
+    // .attr("class", "line-salida") // Estilos
+    // .attr("d", lineSalida); // Llamar al generador de lineas
 
     // Dibujar circulos Entrada
     svg.selectAll(".dotEntrada")
     .data(data)
     .enter().append("circle") // Utiliza append para agregar circulos
-    .attr("class", "dot") // Estilos
+    // .attr("class", "dot") // Estilos
     .attr("cx", function(d, i) { return xScale(d.nota) })
     .attr("cy", function(d) { return yScale(d.ranking) })
-    .attr("r", 5);
+    .attr("r", (d) => radioScale(d.ranking))
+    .style("fill", (d) => `rgb(0, ${colorScale(d.ranking)}, ${colorScale(d.ranking)})`)
+    .on("mouseover", (d) => {
+        tooltip.text(d.nota + " : " + d.ranking);
+        return tooltip.style("visibility", "visible");
+      })
+    .on("mousemove", function () {
+        console.log('mousemove')
+        return tooltip
+          .style("top", d3.event.pageY - 10 + "px")
+          .style("left", d3.event.pageX + 10 + "px");
+      });
 
   
-    // var elementSVG = d3
-    //   .select("#examen")
-    //   .append("svg")
-    //   .attr("width", width)
-    //   .attr("height", height)
-    //   .attr("transform", "translate(0,0)");
-  
-    // (margin = { top: 20, right: 20, bottom: 30, left: 80 });
-    // // (margin = { top: 20, right: 20, bottom: 30, left: 80 }),
-    // //   (width = 960 - margin.left - margin.right),
-    // //   (height = 1000 - margin.top - margin.bottom);
-  
-    // var x = d3.scaleLinear().range([0, width]);
-    // var y = d3.scaleBand().range([height, 0]);
-    // ///////////
-  
-    // var chart = elementSVG
-    //   .append("g")
-    //   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-  
-    // var x = d3
-    //   .scaleLinear()
-    //   .range([0, width * 0.95])
-    //   .domain([
-    //     0,
-    //     d3.max(data, function (d) {
-    //       return d.Valor;
-    //     }),
-    //   ]);
-  
-    // var y = d3
-    //   .scaleBand()
-    //   .range([height, 0])
-    //   .domain(
-    //     data.map(function (d) {
-    //       return d.Parametro;
-    //     })
-    //   )
-    //   .padding(0.3);
-  
-    // chart.append("g").call(d3.axisLeft(y));
-  
-    // chart
-    //   .selectAll("rect")
-    //   .data(data)
-    //   .enter()
-    //   .append("rect")
-    //   .attr("x", 0)
-    //   .attr("height", y.bandwidth())
-    //   .attr("y", function (d) {
-    //     return y(d.Parametro);
-    //   })
-    //   .attr("width", function (d) {
-    //     return x(d.Valor);
-    //   })
-    //   .attr("fill", (d) => color(d.Parametro))
-    //   .on("mouseover", (d) => {
-    //     tooltip.text(d.Parametro + " : " + d.Valor.toFixed(2));
-    //     return tooltip.style("visibility", "visible");
-    //   })
-    //   .on("mousemove", function () {
-    //     return tooltip
-    //       .style("top", d3.event.pageY - 10 + "px")
-    //       .style("left", d3.event.pageX + 10 + "px");
-    //   })
-    //   .on("mouseout", () => tooltip.style("visibility", "hidden"));
-  
-    // var tooltip = d3
-    //   .select("#barchart")
-    //   .append("div")
-    //   .attr("class", "tooltip")
-    //   .style("position", "absolute")
-    //   .style("z-index", 10)
-    //   .style("visibility", "hidden")
-    //   .text("Simple text");
+    var tooltip = d3
+    .select("#examen")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("position", "absolute")
+    .style("z-index", 10)
+    .style("visibility", "hidden")
+    .text("Tooltip");
   });
   
